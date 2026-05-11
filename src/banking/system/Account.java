680c8d9bdc8438802022331d404a4dbda07b4b16
brent <brentjamesphz@gmail.com>
2026-05-12 00:45:32 +0800

    feat: added deposit; sets transactionType & transactionAmount

--- src/banking/system/Account.java
+++ src/banking/system/Account.java
@@ -5,14 +5,14 @@ public class Account {
     
     private double balance;
     private String transactionType;
-
+    private double transactionAmount;
     
     public String getTransactionType() {
         return transactionType;
     }
     
-    public void setTransactionType(String t) {
-        this.transactionType = t;
+    public double getTransactionAmount() {
+        return transactionAmount;
     }
 
     public Account(double balance) {
@@ -23,9 +23,23 @@ public class Account {
         return balance;
     }
     
+    public boolean deposit(double amount) {
+        if(amount > 0){
+            balance += amount;
+            transactionAmount = amount;
+            transactionType = "deposit";
+            
+            return true;
+        }
+        return false;
+    }
+    
     public boolean withdraw(double amount) {
         if (amount <= balance && amount > 0) {
             balance -= amount;
+            transactionAmount = amount;
+            transactionType = "withdraw";
+            
             return true;
         }
         return false;
